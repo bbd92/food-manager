757a77861f692b80c9ffcb327ed49e70fc6dfd37
bbd <bbd.92@icloud.com>
2019-06-01 13:13:15 +0900

    add function of basement

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,14 @@
 class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    Category.create(name:category_params[:name], user_id:current_user.id)
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,28 @@
 class FoodsController < ApplicationController
   before_action :authenticate_user!
+  
   def index
+    @foods = Food.where(user_id: current_user.id)
   end
+  
+  def new
+    @food = Food.new
+  end
+
+  def create
+    Food.create(name:food_params[:name], category_id:food_params[:category_id], quantity:food_params[:quantity], expiration_date:food_params[:expiration_date], user_id:current_user.id)
+  end
+
+  def update
+    if food.uopdate()
+      redirect_to root_path
+    else
+
+    end
+  end
+
+  private
+    def food_params
+      params.require(:food).permit(:name, :category_id, :quantity, :expiration_date)
+    end
 end
